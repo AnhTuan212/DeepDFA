1dc09d9fb7060eaf6d98e974d6fd45b05aec5a21
tuantuan121 <natvn313@gmail.com>
2025-03-14 23:05:03 +0700

    Update download_all.sh

--- scripts/download_all.sh
+++ scripts/download_all.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Raw Big-Vul dataset
-curl -Lo MSR_data_cleaned.zip 'https://figshare.com/ndownloader/files/43990908'
+curl -Lo MSR_data_cleaned.zip 'https://drive.google.com/file/d/1QOiucs8B1hA1g92TxIUdTFNZeAjFKT2K/view?usp=drive_link'
 unzip MSR_data_cleaned.zip -d DDFA/storage/external/
 
 # LineVul version of Big-Vul dataset
-curl -Lo MSR_LineVul.zip 'https://figshare.com/ndownloader/files/43991823'
+curl -Lo MSR_LineVul.zip 'https://drive.google.com/file/d/11G_E4gn1XCAZtwsSfRzkNuhQ4L74dCqk/view?usp=drive_link'
 unzip MSR_LineVul.zip -d LineVul/data/MSR
 
 # DeepDFA preprocessed data
@@ -14,5 +14,5 @@ curl -Lo preprocessed_data.zip 'https://figshare.com/ndownloader/files/43991910'
 unzip preprocessed_data.zip
 
 # DeepDFA CFGs
-curl -Lo before.zip 'https://figshare.com/ndownloader/files/43916550'
+curl -Lo before.zip 'https://drive.google.com/file/d/1KK2b_AxwOxhIEFVO89gp5CLbxiicXKXu/view?usp=drive_link'
 unzip before.zip -d DDFA/storage/processed/bigvul
