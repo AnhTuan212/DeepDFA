47efe77fa7b8f4489ff9bd7c1b0fcc01600c1d2e
tuantuan121 <natvn313@gmail.com>
2025-03-15 10:43:16 +0700

    Update download_all.sh

--- scripts/download_all.sh
+++ scripts/download_all.sh
@@ -2,17 +2,17 @@
 set -e
 
 # Raw Big-Vul dataset
-curl -Lo MSR_data_cleaned.zip 'https://drive.google.com/file/d/1QOiucs8B1hA1g92TxIUdTFNZeAjFKT2K/view?usp=drive_link'
+gdown.download_folder('https://drive.google.com/file/d/1QOiucs8B1hA1g92TxIUdTFNZeAjFKT2K/view?usp=drive_link',quiet = True)
 unzip MSR_data_cleaned.zip -d DDFA/storage/external/
 
 # LineVul version of Big-Vul dataset
-curl -Lo MSR_LineVul.zip 'https://drive.google.com/file/d/11G_E4gn1XCAZtwsSfRzkNuhQ4L74dCqk/view?usp=drive_link'
+gdown.download_folder('https://drive.google.com/file/d/11G_E4gn1XCAZtwsSfRzkNuhQ4L74dCqk/view?usp=drive_link',quiet = True)
 unzip MSR_LineVul.zip -d LineVul/data/MSR
 
 # DeepDFA preprocessed data
-curl -Lo preprocessed_data.zip 'https://figshare.com/ndownloader/files/43991910'
+gdown.download_folder('https://figshare.com/ndownloader/files/43991910',quiet = True)
 unzip preprocessed_data.zip
 
 # DeepDFA CFGs
-curl -Lo before.zip 'https://drive.google.com/file/d/1KK2b_AxwOxhIEFVO89gp5CLbxiicXKXu/view?usp=drive_link'
+gdown.download_folder('https://drive.google.com/file/d/1KK2b_AxwOxhIEFVO89gp5CLbxiicXKXu/view?usp=drive_link',quiet = True)
 unzip before.zip -d DDFA/storage/processed/bigvul
