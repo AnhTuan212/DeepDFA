40e15bc72909402f4430cc6e55039e3133f5bd50
tuantuan121 <natvn313@gmail.com>
2025-03-15 10:45:29 +0700

    Update download_all.sh

--- scripts/download_all.sh
+++ scripts/download_all.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-# Raw Big-Vul dataset
-gdown.download_folder('https://drive.google.com/file/d/1QOiucs8B1hA1g92TxIUdTFNZeAjFKT2K/view?usp=drive_link',quiet = True)
-unzip MSR_data_cleaned.zip -d DDFA/storage/external/
+echo "🔽 Tải Raw Big-Vul dataset..."
+gdown --id 1QOiucs8B1hA1g92TxIUdTFNZeAjFKT2K --quiet -O MSR_data_cleaned.zip
+unzip -o MSR_data_cleaned.zip -d DDFA/storage/external/
 
-# LineVul version of Big-Vul dataset
-gdown.download_folder('https://drive.google.com/file/d/11G_E4gn1XCAZtwsSfRzkNuhQ4L74dCqk/view?usp=drive_link',quiet = True)
-unzip MSR_LineVul.zip -d LineVul/data/MSR
+echo "🔽 Tải LineVul dataset..."
+gdown --id 11G_E4gn1XCAZtwsSfRzkNuhQ4L74dCqk --quiet -O MSR_LineVul.zip
+unzip -o MSR_LineVul.zip -d LineVul/data/MSR/
 
-# DeepDFA preprocessed data
-gdown.download_folder('https://figshare.com/ndownloader/files/43991910',quiet = True)
-unzip preprocessed_data.zip
+echo "🔽 Tải DeepDFA preprocessed data..."
+wget --quiet --show-progress --output-document=preprocessed_data.zip "https://figshare.com/ndownloader/files/43991910"
+unzip -o preprocessed_data.zip
 
-# DeepDFA CFGs
-gdown.download_folder('https://drive.google.com/file/d/1KK2b_AxwOxhIEFVO89gp5CLbxiicXKXu/view?usp=drive_link',quiet = True)
-unzip before.zip -d DDFA/storage/processed/bigvul
+echo "🔽 Tải DeepDFA CFGs..."
+gdown --id 1KK2b_AxwOxhIEFVO89gp5CLbxiicXKXu --quiet -O before.zip
+unzip -o before.zip -d DDFA/storage/processed/bigvul/
+
+echo "✅ Hoàn thành tải và giải nén tất cả dữ liệu!"
