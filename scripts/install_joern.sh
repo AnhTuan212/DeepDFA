590659ae37e08ca2c6333b953bbca4619dd1b5ed
tuantuan121 <natvn313@gmail.com>
2025-03-15 17:40:22 +0700

    ok

--- scripts/install_joern.sh
+++ scripts/install_joern.sh
@@ -2,7 +2,7 @@
 # Version 1.1.107 was used for the paper
 mkdir joern
 cd joern
-curl -L "https://github.com/joernio/joern/releases/latest/download/joern-install.sh" -o joern-install.sh
+curl -L "https://github.com/joernio/joern/releases/download/v1.1.107/joern-install.sh" -o joern-install.sh
 chmod u+x joern-install.sh
 # now run the script with the following options
 printf "y\\n$PWD/joern\\nn\\nv1.1.107\\n" | ./joern-install.sh --interactive --without-plugins
